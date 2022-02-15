1e0f26d5f23040674b232d65f48663963ab92ca6
tiy1807 <thomasyems+github@gmail.com>
2022-02-15 21:20:42 +0000

    Created version 4 and made error messaging more efficient

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 router.use('/juggling/current/', require('./views/juggling/current/_routes'))
+router.use('/juggling/4/', require('./views/juggling/4/_routes'))
+router.use('/juggling/3/', require('./views/juggling/3/_routes'))
 router.use('/juggling/2/', require('./views/juggling/2/_routes'))
 router.use('/juggling/1/', require('./views/juggling/1/_routes'))
 
--- app/views/juggling/4/_routes.js
+++ app/views/juggling/4/_routes.js
@@ -8,31 +8,27 @@ function getErrorNameFromID(id) {
     }).join('');
 }
 
-function addError(errors, id, errorMessage, href="") {
-  error.errorList.push({
-      text: errorMessage,
-      href: "#" + id + href
-      })
-  error[getErrorNameFromID(id)] = {text: errorMessage}
-}
-
-function checkCompleted(req, errors, id, errorMessage) {
-  if (!req.session.data[id]) {
-    addError(errors, id, errorMessage)
-  }
-}
-
 // Run this code when a form is submitted to 'juggling-balls-answer'
 router.post('/juggling-balls-submit', function (req, res) {
   // Create a variable to hold the errors
   var error = {errorList: []}
 
-  checkCompleted(req, error, 'how-many-balls', "Select the number of balls you can juggle")
+  // Make a variable and give it the value from 'how-many-balls'
+  var id = "how-many-balls"
+  var howManyBalls = req.session.data[id]
+
+  if (!howManyBalls) {
+    var errorMessage = "Select the number of balls you can juggle"
+    error.errorList.push({
+      text: errorMessage,
+      href: "#how-many-balls"
+      })
+    error[getErrorNameFromID(id)] = {text: errorMessage}
+  }
 
   if (error.errorList.length > 0) {
     res.render(req.baseUrl.substring(1) + '/juggling-balls', error)
   } else {
-      var howManyBalls = req.session.data['how-many-balls']
       // Check whether the variable matches a condition
       if (howManyBalls == "3 or more"){
         // Send user to next page
@@ -49,7 +45,17 @@ router.post('/juggling-trick-submit', function (req, res) {
   var error = {errorList: []}
 
   // Make a variable and give it the value from 'how-many-balls'
-  checkCompleted(req, error, 'most-impressive-trick', "Enter a description of your most impressive trick")
+  var id = "most-impressive-trick"
+  var mostImpressiveTrick = req.session.data[id]
+
+  if (!mostImpressiveTrick) {
+    var errorMessage = "Enter a description of your most impressive trick"
+    error.errorList.push({
+      text: errorMessage,
+      href: "#" + id
+      })
+    error[getErrorNameFromID(id)] = {text: errorMessage}
+  }
 
   if (error.errorList.length > 0) {
     res.render(req.baseUrl.substring(1) + '/juggling-trick', error)
