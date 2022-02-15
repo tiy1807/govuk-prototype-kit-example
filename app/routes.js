4ae44a9a88282660351cdd79b944ba69aa91f566
tiy1807 <thomasyems+github@gmail.com>
2022-02-15 20:43:07 +0000

    Added base templates

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.use('/juggling/current/', require('./views/juggling/current/_routes'))
 router.use('/juggling/1/', require('./views/juggling/1/_routes'))
 
 module.exports = router
