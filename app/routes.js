e63b359c7ba3f022af43804bf7332d8b3b61c01a
tiy1807 <thomasyems+github@gmail.com>
2022-02-15 20:10:09 +0000

    Added branching
    Added branching to version 1

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.use('/juggling/1/', require('./views/juggling/1/_routes'))
 
 module.exports = router
