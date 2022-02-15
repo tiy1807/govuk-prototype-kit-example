e391f2d54e152341c366e6fb67a646f61fd018c6
tiy1807 <thomasyems+github@gmail.com>
2022-02-15 20:50:55 +0000

    Saved version 2, added default values to base template

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 router.use('/juggling/current/', require('./views/juggling/current/_routes'))
+router.use('/juggling/2/', require('./views/juggling/2/_routes'))
 router.use('/juggling/1/', require('./views/juggling/1/_routes'))
 
 module.exports = router
